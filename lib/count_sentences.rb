d4cdc752c64e5aba134bbbb24c496f8d8a5f9fd0
Laura Berge <lauraeberge@gmail.com>
2019-09-10 19:22:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,13 +16,10 @@ class String
 
   def count_sentences
     i = 0
-    (i += (self.split(".").count)) if sentence?
+    (i += self.split(".").count) if sentence?
     (i += self.split("?").count) if question?
     (i += self.split("!").count) if exclamation?
-    
-#    if sentence? || question? || exclamation?
-#      i = (self.split(".").count) + (self.split("?").count) + (self.split("!").count) - 3
-#    end
+
     i
   end
 end
